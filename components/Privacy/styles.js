b2a49de79cafce8d811b12afa09e089c91bcab35
Jay Fallon <jayfallon@gmail.com>
2019-03-25 07:30:18 -0400

    customized terms and privacy

---
+++ components/Privacy/styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const PrivacyStyles = styled.section`
+  margin: ${props => props.theme.pageMargin};
+  width: ${props => props.theme.pageWidth};
+
+  h2 {
+    padding: 2rem 0;
+    font-family: source-serif-pro serif;
+    font-size: 2.9rem;
+  }
+
+  h3 {
+    padding: 1.6rem 0;
+    font-family: source-serif-pro, serif;
+    font-size: 2.6rem;
+  }
+
+  h4 {
+    padding: 1.3rem 0;
+    font-size: 2rem;
+    font-weight: 700;
+  }
+
+  ul {
+    padding: 1.5rem 0 1.5rem 2rem;
+  }
+
+  ul > li {
+    list-style: disc;
+  }
+`;
+
+export default PrivacyStyles;
