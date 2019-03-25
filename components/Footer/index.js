67a56dadab3558724780fb176b5f9dd028cf12d8
Jay Fallon <jayfallon@gmail.com>
2019-03-25 07:49:50 -0400

    customized footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,19 +4,23 @@ import FooterStyles from './styles';
 
 const Footer = () => (
   <FooterStyles>
-    <p>this is the footer</p>
-    <ul>
-      <li>
-        <Link href="/terms">
-          <a title="Terms and Conditions">Terms &amp; Conditions</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/privacy">
-          <a title="Privacy Policy">Privacy Policy</a>
-        </Link>
-      </li>
-    </ul>
+    <div className="footer__inner">
+      <nav>
+        <ul>
+          <li>
+            <Link href="/terms">
+              <a title="Terms and Conditions">Terms &amp; Conditions</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/privacy">
+              <a title="Privacy Policy">Privacy Policy</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="footer__copyright">{'\u00A9'}2019-Allicin</div>
+    </div>
   </FooterStyles>
 );
 
